a109d535742b84557304f60316c1a79e62efe0ee
ANZ JO XAVIER <116029351+anzjoxavier@users.noreply.github.com>
2023-02-28 01:20:14 +0530

    Step29

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ import 'package:simpleproject/views/verify_email.dart';
 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.lime,
